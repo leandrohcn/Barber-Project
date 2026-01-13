6811e69d2353b65b3b4f93dc2a21d5df05cc7284
leandrohcn <leandro1111bfr@gmail.com>
2026-01-13 16:31:08 -0300

    fix: Configurações do projeto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './db/prisma.module';
 import { CatalogModule } from './domain/Catalogo/catalog.module';
+import { UsersModule } from './domain/Users/users.module';
 
 @Module({
-  imports: [PrismaModule, CatalogModule],
+  imports: [PrismaModule, CatalogModule, UsersModule],
 })
 export class AppModule {}
