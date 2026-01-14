7aab04341dbd938a4847fc518c290c660cc66d6f
leandrohcn <leandro1111bfr@gmail.com>
2026-01-14 15:03:06 -0300

    fix: modulo do agendamento

--- backend/src/domain/agendamentos/agendamentos.module.ts
+++ backend/src/domain/agendamentos/agendamentos.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AgendamentosService } from './agendamentos.service';
 import { AgendamentosController } from './agendamentos.controller';
+import { PrismaModule } from 'src/db/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AgendamentosController],
   providers: [AgendamentosService],
 })
