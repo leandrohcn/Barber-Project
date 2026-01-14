db7f1ae1455f8ddf540ef010499325eac435cd7f
leandrohcn <leandro1111bfr@gmail.com>
2026-01-14 13:57:13 -0300

    fix: correção no auth para uma melhor resposta do token

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { CatalogModule } from './domain/Catalogo/catalog.module';
 import { UsersModule } from './domain/Users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { AgendamentosModule } from './domain/agendamentos/agendamentos.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule, CatalogModule, UsersModule, AuthModule],
+    PrismaModule, CatalogModule, UsersModule, AuthModule, AgendamentosModule],
 })
 export class AppModule {}
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // <--- Importe o Config
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from 'src/db/prisma.module';
 import { UsersModule } from 'src/domain/Users/users.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+    PrismaModule,
+    ConfigModule,
+    UsersModule, 
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'), // <--- Aqui ele busca do .env com segurança
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
     }),
   ],
-  providers: [AuthService],
   controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
