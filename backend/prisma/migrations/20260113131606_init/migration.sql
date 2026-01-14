521526ff4b426a61c401e8b0eeef97ec8a65c515
leandrohcn <leandro1111bfr@gmail.com>
2026-01-13 21:51:52 -0300

    feat: Implementação do Next inicial

--- backend/prisma/migrations/20260113131606_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Service" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "price" REAL NOT NULL,
-    "duration" INTEGER NOT NULL
-);
